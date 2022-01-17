ae5744182e71672449fd1b42fc803c61e8e9eefc
realbenben <b.patou59@gmail.com>
2022-01-17 11:08:01 +0100

    first commit

---
+++ HelloWorld/src/com/openclassrooms/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.openclassrooms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+	@Test
+	public void testAjouter() {
+	    HelloWorld testHelloWorld = new HelloWorld();
+	    int somme = testHelloWorld.sommesNombres(2, 3);
+	    assertEquals(5, somme);
+	}
+
+}
---
+++ exempleMaven/src/main/java/com/openClassrom/installeride/ExempleMaven.java
@@ -0,0 +1,16 @@
+package com.openClassrom.installeride;
+
+import ij.IJ;
+import ij.ImagePlus;
+
+
+public class ExempleMaven {
+
+	public static void main(String[] args) {
+	ImagePlus imp = IJ.openImage();
+//	choisi le fichier et stock le result dans imp 
+	imp.show();
+//	affiche l image a l'ecran
+	}
+
+}
